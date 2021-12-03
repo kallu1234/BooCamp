9c501fffff02eca8650b22bc2c47b7b6d93b4a7d
Kalyani Vilas Dudhekar <62753917+kallu1234@users.noreply.github.com>
2021-12-03 23:28:56 +0530

    Add files via upload

--- Bootcamp_Notes/Dp_Problems/Min_Cost.py
+++ Bootcamp_Notes/Dp_Problems/Min_Cost.py
@@ -2,34 +2,24 @@
 #code for minimum cost climbing stairs
 class Solution:
     def minCostClimbingStairs(self, cost):
-        #here we can start the currentStair from 0 or 1 and here we have to find the min cost for climbing 
         a=self.minCost(cost,0,{})
         b=self.minCost(cost,1,{})
         
         return min(a,b)
     
     def minCost(self,cost,currIndex,memo):
-        #here if currIndex is equal to length of cost then return 0
         if currIndex==len(cost):
             return 0
-        
-        #here if currIndex is greater return 10000 or max_int value bcoz here we are finding minimum value 
         if currIndex>len(cost):
             return 1000
         
-        #create a key of it 
         currKey=currIndex
-        
-        #check if currKey is present in memo or not 
         if currKey in memo:
             return memo[currKey]
         
-        
-        #calculate the cost of each stairs 
         onejump=cost[currIndex]+self.minCost(cost,currIndex+1,memo)
         twojump=cost[currIndex]+self.minCost(cost,currIndex+2,memo)
         
-        #calculate the min cost of 1 jump and 2 jump 
         memo[currKey]=min(onejump,twojump)
         return memo[currKey]
         
@@ -41,10 +31,3 @@ s=Solution()
 cost=[10,15,20]
 ans=s.minCostClimbingStairs(cost)
 print(ans)
-
-
-# Input: cost = [10,15,20]
-# Output: 15
-# Explanation: You will start at index 1.
-# - Pay 15 and climb two steps to reach the top.
-# The total cost is 15.
--- Bootcamp_Notes/Dp_Problems/perfect_square.py
+++ Bootcamp_Notes/Dp_Problems/perfect_square.py
@@ -1,39 +1,30 @@
-from math import sqrt,floor
-memo={}
 class Solution:
     def numSquares(self, n):
-        # if n<=3:
-        #     return n
-        # else:
-        s=floor(sqrt(n+1))
-        nums=[x**2 for x in range(1,s+1)]
-       
+        perfect=[]
+        for i in range(1,n+1):
+            perfect.append(i*i)
+        return self.LeastNum(0,n,perfect,{})               
         
-        return self.find(nums,0,n)
-     
-    def find(self,nums,currIndex,target):
-        if currIndex>=len(nums) and target!=0:
-            return 1000
+    def LeastNum(self,currIndex,target,perfect,memo):
         if target==0:
             return 0
-        consider=10000
-        key=(currIndex,target)
+        if currIndex>=len(perfect):
+                return 1000
+            
+        key=str(currIndex)+"->"+str(target)
         if key in memo:
             return memo[key]
+        consider=1000
         
-        # if currIndex==0 and nums[currIndex+1]>target:
-        #     consider=1000
-        
-        if nums[currIndex]<=target:
-            consider=1+self.find(nums,currIndex,target-nums[currIndex])
-        notconsider=self.find(nums,currIndex+1,target)
-        memo[key]=min(consider,notconsider)
+        if perfect[currIndex]<=target:
+            
+            consider=1+self.LeastNum(currIndex,target-perfect[currIndex],perfect,memo)
+        notconsider=self.LeastNum(currIndex+1,target,perfect,memo)
+        memo[key]= min(consider,notconsider)
         return memo[key]
-        
-        
-    
-        
+
+
+n=int(input("Enter a value"))
 s=Solution()
-n=12
 ans=s.numSquares(n)
 print(ans)
